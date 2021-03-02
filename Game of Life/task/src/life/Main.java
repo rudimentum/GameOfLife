df1c8afeb559869a8a8d754ee311e8a1dc20a785
Victoria Grinberg <31312528+ramquist@users.noreply.github.com>
2021-03-02 21:25:31 +0300

    Update Main.java
    Create a “map” of the universe in its first moment of existence ― the first generation

--- Game of Life/task/src/life/Main.java
+++ Game of Life/task/src/life/Main.java
@@ -1,7 +1,26 @@
 package life;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int universeSize = scanner.nextInt();
+        int seed = scanner. nextInt();
+        startUniverse(universeSize, seed);
+    }
 
+    public static void startUniverse(int size, int seed) {
+        Random random = new Random(seed);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (random.nextBoolean())
+                    System.out.print("O");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
